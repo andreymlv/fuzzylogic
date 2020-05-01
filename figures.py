a569c4c396fefb55450341c3a263f97a1a082f43
poshekhon <shapehonwork@gmail.com>
2020-05-01 17:45:34 +0300

    Added trapezoid class

--- figures.py
+++ figures.py
@@ -1,74 +1,103 @@
 
 
 class Triangle():
+	"""
+	Этот класс используется для нахождения степени нечёткого равенства 
+	методом треугольника
+	"""
 
-    def __init__(self, lower, upper, peak, x):
-        self.lower = lower
-        self.upper = upper
-        self. peak = peak
-        self.x = x
+	def __init__(self, lower, upper, peak, x):
+		"""Иницилизация объекта"""
+		self.lower = lower
+		self.upper = upper
+		self. peak = peak
+		self.x = x
 
-        self.a = self.upper - self.lower
-        self.u = self.mftriangle()
-    
-    def mftriangle(self):
-        u = 0
-        if ((self.x >= self.lower) and (self.x <= self.peak)):
-            u = 1 - ((self.peak - self.x) / (self.peak - self.lower))
-            return u
-        if ((self.x >= self.peak) and (self.x <= self.upper)):
-            u = 1 - ((self.x - self.peak) / (self.upper - self.peak))
-            return u
-        return u
+		self.a = self.upper - self.lower
+		self.u = self.mftriangle()
+	
+	def mftriangle(self):
+		"""Функция принадлежности"""
+		u = 0
+		if ((self.x >= self.lower) and (self.x <= self.peak)):
+			u = 1 - ((self.peak - self.x) / (self.peak - self.lower))
+			return u
+		if ((self.x >= self.peak) and (self.x <= self.upper)):
+			u = 1 - ((self.x - self.peak) / (self.upper - self.peak))
+			return u
+		return u
 
-    def get_coords(self):    
+	def get_coords(self):
+		"""
+		Функция генерирования координат на основе найденой 
+		степени нечёткого равенства
+		"""
+		print(self.u)
 
-        print(self.u)
-        
-        """
-        everywhere add by +a*i
-        x_list = [self.lower, self.lower+self.upper-x[i], x[i], self.upper]
-        y_list = [0, u, u, 0]
-        """
+		if (self.u != 1.0):
+			x_list = sorted([self.lower, self.lower+self.upper-self.x, self.x, self.upper])
+			y_list = [0, self.u, self.u, 0]
+		else:
+			x_list = sorted([self.lower, self.x, self.upper])
+			y_list = [0, self.u, 0]
 
-        if (self.u != 1.0):
-            x_list = sorted([self.lower, self.lower+self.upper-self.x, self.x, self.upper])
-            y_list = [0, self.u, self.u, 0]
-        else:
-            x_list = sorted([self.lower, self.x, self.upper])
-            y_list = [0, self.u, 0]
+		coords = list()
+		coords.append(x_list)
+		coords.append(y_list)
 
-        coords = list()
-        coords.append(x_list)
-        coords.append(y_list)
+		return coords
 
-        return coords
-
-    
-    def make_plots(self):
-        if (self.u == 1):
-            plt.plot([lower+a*i, x[i]+a*i, upper+a*i], [0, 1, 0])
-        else:
-            plt.plot(sorted(x_list), y_list)
+	
+	def make_plot(self, ox, oy, ax):
+		"""Создание графика нечёткого равенства"""
+		ax.plot(sorted(ox), oy)
 
 
 class Trapezoid():
+	"""
+	Этот класс используется для нахождения степени нечёткого равенства 
+	методом трапеции
+	"""
 
-    def __init__(self, lower, upper, first_peak, second_peak, x):
-        pass
+	def __init__(self, lower, upper, first_peak, second_peak, x):
+		self.lower = lower
+		self.upper = upper
+		self.first_peak = first_peak
+		self.second_peak = second_peak
+		self.x = x
 
-    def mftrapezoid(self):
-        u = 0
-        if ((x >= lower) and (x <= peak_lower)):
-            u = 1 - ((peak_lower - x) / (peak_lower - lower))
-            return u
-        if (x >= peak_lower) and (x <= peak_upper):
-            u = 1
-            return u
-        if ((x >= peak_upper) and (x <= upper)):
-            u = 1 - ((x - peak_upper) / (upper - peak_upper))
-            return u
-        return u
+		self.a = self.upper - self.lower
+		self.u = self.mftrapezoid()
 
-    def get_coords(self):
-        pass
+	def mftrapezoid(self):
+		u = 0
+		if ((self.x >= self.lower) and (self.x <= self.first_peak)):
+			u = 1 - ((self.first_peak - self.x) / (self.first_peak - self.lower))
+			return u
+		if (self.x >= self.first_peak) and (self.x <= self.second_peak):
+			u = 1
+			return u
+		if ((self.x >= self.second_peak) and (self.x <= self.upper)):
+			u = 1 - ((self.x - self.second_peak) / (self.upper - self.second_peak))
+			return u
+		return u
+
+	def get_coords(self):
+		"""
+		Функция генерирования координат на основе найденой 
+		степени нечёткого равенства
+		"""
+		print(self.u)
+
+		x_list = sorted([self.lower, self.lower+self.upper-self.x, self.x, self.upper])
+		y_list = [0, self.u, self.u, 0]
+
+		coords = list()
+		coords.append(x_list)
+		coords.append(y_list)
+
+		return coords
+
+	def make_plot(self, ox, oy, ax):
+		"""Создание графика нечёткого равенства"""
+		ax.plot(sorted(ox), oy)
--- main.py
+++ main.py
@@ -2,40 +2,12 @@
 import matplotlib.pyplot as plt
 import figures as fg
 
-
-
-def print_trapezoid(args):
-
-    lower = args[1]
-    upper = args[2]
-    a = upper - lower
-    peak1 = args[3]
-    peak2 = args[4]
-    x = args[-1]
-    len_of_x = len(x)
-
-    for i in range(len_of_x):
-
-        u = trapezoid(upper, lower, peak1, peak2, x[i])
-
-        x_list = [lower+a*i, upper-x[i]+lower+a*i, x[i]+a*i, upper+a*i]
-        y_list = [0, u, u, 0]
-
-        if (u == 1):
-            plt.plot([lower+a*i, peak1+a*i, peak2 +
-                      a*i, upper+a*i], [0, 1, 1, 0])
-        else:
-            plt.plot(sorted(x_list), y_list)
-
-
 def main():
     args = [2, 10, 6]
     x = list()
     triangles = list()
 
     fig, ax = plt.subplots()
-
-
 
     for i in range(int(input("Enter number of X's => "))):
         x_current = float(input("Enter x => "))
@@ -48,10 +20,8 @@ def main():
         for j in range(len(coords[0])):
             coords[0][j] += triangles[i].a * i
 
-        ax.plot(coords[0], coords[1])
-
-
-    # print_trapezoid(take_input())
+        triangles[i].make_plot(coords[0], coords[1], ax)
+    
     plt.show()
 
 
