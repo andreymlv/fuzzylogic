4d15b632f9ea837185e70daf11127ae04e0df7b2
poshekhon <shapehonwork@gmail.com>
2020-05-02 00:39:16 +0300

    Formattig file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import figures as fg
 
+
 def main():
     args = [2, 10, 6]
     x = list()
@@ -13,15 +14,15 @@ def main():
         x_current = float(input("Enter x => "))
         x.append(x_current)
         triangles.append(fg.Triangle(*args, x[i]))
-    
+
     for i in range(len(triangles)):
         coords = triangles[i].get_coords()
-        
+
         for j in range(len(coords[0])):
             coords[0][j] += triangles[i].a * i
 
         triangles[i].make_plot(coords[0], coords[1], ax)
-    
+
     plt.show()
 
 
